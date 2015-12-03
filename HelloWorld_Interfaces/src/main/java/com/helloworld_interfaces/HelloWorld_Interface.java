d38cd59bca89a0b84feba1ace3957e218ef4327b
RodolfoR <rodolfo_una@hotmail.com>
2015-12-03 08:36:02 -0300

    Com alterações

--- HelloWorld_Interfaces/src/main/java/com/helloworld_interfaces/HelloWorld_Interface.java
+++ HelloWorld_Interfaces/src/main/java/com/helloworld_interfaces/HelloWorld_Interface.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.testandojpa.helloworld_interfaces;
+package com.helloworld_interfaces;
 
 /**
  *
